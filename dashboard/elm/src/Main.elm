702df3345a435d9c4e8433d610f1291bb1ca8edd
Cyber Oliveira <lccro@msn.com>
2021-09-23 00:03:33 -0300

    adiciona elm.animation nos cards da perola

--- dashboard/elm/src/Main.elm
+++ dashboard/elm/src/Main.elm
@@ -1,20 +1,20 @@
 port module Main exposing (..)
 
+import Animation exposing (percent, px)
+import Animation.Messenger
 import Browser
-import Dict exposing (Dict)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Decode as D
-import Process
-import Task
+import Time
 
 
 
 -- MAIN
 
 
-main : Program () Model Msg
+main : Program Float Model Msg
 main =
     Browser.element
         { init = init
@@ -51,18 +51,19 @@ type alias Card =
     { username : String
     , text : String
     , user_picture : String
+    , animStyle : Animation.Messenger.State Msg
     }
 
 
 type alias Model =
-    { cards : Dict Int Card
-    , card_id : Int
+    { cards : List Card
+    , innerHeight : Float
     }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Model Dict.empty 0, Cmd.none )
+init : Float -> ( Model, Cmd Msg )
+init innerHeight =
+    ( Model [] innerHeight, Cmd.none )
 
 
 
@@ -71,38 +72,56 @@ init _ =
 
 type Msg
     = Recv String
-    | Remove Int
+    | Animate Animation.Msg
+    | Finished
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Remove id ->
+        Animate animMsg ->
             let
-                new_dict =
-                    Dict.remove id model.cards
+                stylesAndCmds =
+                    List.map (\card -> Animation.Messenger.update animMsg card.animStyle) model.cards
+
+                styles =
+                    List.map Tuple.first stylesAndCmds
+
+                cmds =
+                    List.map Tuple.second stylesAndCmds
+
+                updateStyle card style =
+                    { card | animStyle = style }
             in
-            ( { model | cards = new_dict }, Cmd.none )
+            ( { model | cards = List.map2 updateStyle model.cards styles }
+            , Cmd.batch cmds
+            )
 
         Recv json ->
             case D.decodeString websocketMessageDecoder json of
                 Ok ws ->
                     let
-                        next_id =
-                            model.card_id + 1
+                        newAnimation =
+                            Animation.interrupt
+                                [ Animation.to [ Animation.translate (percent 0) (px 0) ]
+                                , Animation.wait (Time.millisToPosix <| 15 * 1000)
+                                , Animation.to [ Animation.translate (percent 115) (percent 0) ]
+                                , Animation.Messenger.send Finished
+                                ]
+                                (Animation.style [ Animation.translate (percent 0) (px model.innerHeight) ])
 
-                        new_dict =
-                            Dict.insert next_id (Card ws.username ws.text ws.user_picture) model.cards
+                        newCard =
+                            Card ws.username ws.text ws.user_picture newAnimation
                     in
-                    ( { model | card_id = next_id, cards = new_dict }
-                    , Cmd.batch
-                        [ playUrl ws.audio_url
-                        , Process.sleep 15000 |> Task.perform (always (Remove next_id))
-                        ]
+                    ( { model | cards = model.cards ++ [ newCard ] }
+                    , playUrl ws.audio_url
                     )
 
                 Err _ ->
                     ( model, Cmd.none )
+
+        Finished ->
+            ( { model | cards = List.drop 1 model.cards }, Cmd.none )
 
 
 
@@ -110,8 +129,11 @@ update msg model =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions _ =
-    messageReceiver Recv
+subscriptions model =
+    Sub.batch
+        [ messageReceiver Recv
+        , Animation.subscription Animate <| List.map .animStyle model.cards
+        ]
 
 
 
@@ -120,10 +142,13 @@ subscriptions _ =
 
 cardView : Card -> Html Msg
 cardView card =
-    div [ class "content" ]
+    div
+        (Animation.render card.animStyle
+            ++ [ class "content" ]
+        )
         [ div [ class "user-picture" ] [ img [ src card.user_picture ] [] ]
         , div [ class "container" ]
-            [ div [ class "username" ] [ text card.username ]
+            [ div [ class "username" ] [ text <| card.username ++ " disse:" ]
             , div [ class "text" ] [ text card.text ]
             ]
         ]
@@ -131,11 +156,7 @@ cardView card =
 
 view : Model -> Html Msg
 view model =
-    let
-        card_list =
-            Dict.values model.cards
-    in
-    div [ class "main" ] (List.map cardView card_list)
+    div [ class "main" ] (List.map cardView model.cards)
 
 
 
